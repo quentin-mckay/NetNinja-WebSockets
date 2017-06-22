2b2d32a4784a4a2d331c34a8f6c987a85a8fc716
Quentin McKay <quentinmckay1@gmail.com>
2017-06-22 14:57:29 +1000

    Installed socket.io (npm install socket.io --save)

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
 
 var server = app.listen(4000, function() {
   console.log('listening to requests on port 4000');
 });
+
+
+//Static files
+app.use(express.static('public'));
