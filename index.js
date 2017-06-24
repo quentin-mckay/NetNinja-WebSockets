5ad84d428bbe6bc35a1c5c691c63a2fa30cb87ce
Quentin McKay <quentinmckay1@gmail.com>
2017-06-24 15:24:07 +1000

    Added broadcast "is typing..."

--- index.js
+++ index.js
@@ -15,10 +15,19 @@ app.use(express.static('public'));
 // Socket setup
 var io = socket(server);
 
+// when a client makes a connection
 io.on('connection', function(socket) {
+  // passed in socket refers to individual socket that is connected
   console.log('made socket connection', socket.id);
 
+  // when we recieve a 'chat' data from the individual
   socket.on('chat', function(data) {
+    // send a 'chat' data to ALL of the connected sockets
     io.sockets.emit('chat', data);
+  })
+
+  socket.on('typing', function(data) {
+    // emit to every other single client
+    socket.broadcast.emit('typing', data);
   })
 })
--- public/chat.js
+++ public/chat.js
@@ -6,16 +6,32 @@ var output = document.querySelector('#output');
 var handle = document.querySelector('#handle');
 var message = document.querySelector('#message');
 var sendButton = document.querySelector('#send');
+var feedback = document.querySelector('#feedback');
 
 // Emit events
 sendButton.addEventListener('click', function() {
   socket.emit('chat', {
-    message:  message.value,
+    message:  message.value,     // .value b/c it's an <input> field
+    handle: handle.value
+  })
+
+})
+
+message.addEventListener('keypress', function() {
+  socket.emit('typing', {
     handle: handle.value
   })
 })
 
-// Listen for events
+// Listen for events coming from server code
+// ...socket is one instance of the websocket in this particular client
 socket.on('chat', function(data) {
   output.innerHTML += `<p><strong>${data.handle}: </strong>${data.message}</p>`;
+
+  // and clear the "... is typing..."
+  feedback.innerHTML = '';
+})
+
+socket.on('typing', (data) => {
+  feedback.innerHTML = `<p><em>${data.handle} is typing a message...</em></p>`;
 })
